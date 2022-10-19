98cde81274c48749aacf808f144eb87cd711955a
Maria Scharin <maria.scharin@comvalresearch.com>
2022-10-19 17:41:00 +0200

    S3 events and user notification part 2: Using websockets to send messages to client

--- src/lambda/s3/sendNotifications.ts
+++ src/lambda/s3/sendNotifications.ts
@@ -1,6 +1,6 @@
 import { S3Handler, S3Event } from 'aws-lambda'
 import 'source-map-support/register'
-/*import * as AWS  from 'aws-sdk'
+import * as AWS  from 'aws-sdk'
 
 const docClient = new AWS.DynamoDB.DocumentClient()
 
@@ -14,13 +14,27 @@ const connectionParams = {
 }
 
 const apiGateway = new AWS.ApiGatewayManagementApi(connectionParams)
-*/
 
 export const handler: S3Handler = async (event: S3Event) => {
   console.log('Processing SNS event ', JSON.stringify(event))
   for (const record of event.Records) {
     const key = record.s3.object.key
     console.log('Processing S3 event with key: ', key)
+
+  const connections = await docClient.scan({
+    TableName: connectionsTable
+  }).promise()
+
+  const payload = {
+    imageId: key
+  }
+
+
+  for (const connection of connections.Items) {
+    const connectionId = connection.id
+    await sendMessageToClient(connectionId, payload)
+  } 
+
     //const s3Event = JSON.parse(s3EventStr)
 
     //await processS3Event(s3Event)
@@ -45,7 +59,7 @@ export const handler: S3Handler = async (event: S3Event) => {
         await sendMessageToClient(connectionId, payload)
     }
   }
-}
+}*/
 
 async function sendMessageToClient(connectionId, payload) {
   try {
@@ -70,4 +84,4 @@ async function sendMessageToClient(connectionId, payload) {
 
     }
   }
-}*/
+}
