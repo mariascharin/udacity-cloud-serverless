bc9fc0faeb3431ea28662cf194bc1a753ceb60a5
Maria Scharin <maria.scharin@comvalresearch.com>
2022-10-21 13:45:54 +0200

    Implement SNS

--- src/lambda/s3/sendNotifications.ts
+++ src/lambda/s3/sendNotifications.ts
@@ -1,4 +1,4 @@
-import { S3Handler, S3Event } from 'aws-lambda'
+import { SNSHandler, SNSEvent, S3Event } from 'aws-lambda'
 import 'source-map-support/register'
 import * as AWS  from 'aws-sdk'
 
@@ -15,33 +15,18 @@ const connectionParams = {
 
 const apiGateway = new AWS.ApiGatewayManagementApi(connectionParams)
 
-export const handler: S3Handler = async (event: S3Event) => {
+export const handler: SNSHandler = async (event: SNSEvent) => {
   console.log('Processing SNS event ', JSON.stringify(event))
-  for (const record of event.Records) {
-    const key = record.s3.object.key
-    console.log('Processing S3 event with key: ', key)
+  for (const snsRecord of event.Records) {
+    const s3EventStr = snsRecord.Sns.Message
+    console.log('Processing S3 event: ', s3EventStr)
+    const s3Event = JSON.parse(s3EventStr)
 
-  const connections = await docClient.scan({
-    TableName: connectionsTable
-  }).promise()
-
-  const payload = {
-    imageId: key
-  }
-
-
-  for (const connection of connections.Items) {
-    const connectionId = connection.id
-    await sendMessageToClient(connectionId, payload)
-  } 
-
-    //const s3Event = JSON.parse(s3EventStr)
-
-    //await processS3Event(s3Event)
+    await processS3Event(s3Event)
   }
 }
 
-/*async function processS3Event(s3Event: S3Event) {
+async function processS3Event(s3Event: S3Event) {
   for (const record of s3Event.Records) {
     const key = record.s3.object.key
     console.log('Processing S3 item with key: ', key)
@@ -59,7 +44,7 @@ export const handler: S3Handler = async (event: S3Event) => {
         await sendMessageToClient(connectionId, payload)
     }
   }
-}*/
+}
 
 async function sendMessageToClient(connectionId, payload) {
   try {
