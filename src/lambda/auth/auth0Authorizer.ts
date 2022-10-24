aba8fedda54f6a792640ca8cc8c0ce9c5ce8d795
Maria Scharin <maria.scharin@comvalresearch.com>
2022-10-24 12:12:13 +0200

    Auth0 as authorizer, but with hard coded secret

--- src/lambda/auth/auth0Authorizer.ts
+++ src/lambda/auth/auth0Authorizer.ts
@@ -1,13 +1,18 @@
 import { CustomAuthorizerEvent, CustomAuthorizerResult, CustomAuthorizerHandler } from 'aws-lambda'
 import 'source-map-support/register'
 
+import { verify } from 'jsonwebtoken'
+import { JwtToken } from '../../auth/JwtToken'
+
+const auth0Secret = process.env.AUTH_0_SECRET
+
 export const handler: CustomAuthorizerHandler = async (event: CustomAuthorizerEvent): Promise<CustomAuthorizerResult> => {
   try {
-    verifyToken(event.authorizationToken)
+    const decodedToken = verifyToken(event.authorizationToken)
     console.log('User was authorized')
 
     return {
-      principalId: 'user',
+      principalId: decodedToken.sub,
       policyDocument: {
         Version: '2012-10-17',
         Statement: [
@@ -38,7 +43,7 @@ export const handler: CustomAuthorizerHandler = async (event: CustomAuthorizerEv
   }
 }
 
-function verifyToken(authHeader: string) {
+function verifyToken(authHeader: string): JwtToken {
   if (!authHeader)
     throw new Error('No authentication header')
 
@@ -48,9 +53,5 @@ function verifyToken(authHeader: string) {
   const split = authHeader.split(' ')
   const token = split[1]
 
-  // Here is where we'll validate the real token later
-  if (token !== '123') 
-    throw new Error('Invalid token')
-
-  // A request has been authorized
+  return verify(token, auth0Secret) as JwtToken
 }
